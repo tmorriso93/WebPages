0c7adce8253974f3d665357c3a5f5fae3ef234dc
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2021-06-24 08:44:42 -0400

    Add files via upload
    University website made with HTML CSS/SASS and a little JavaScript.
    this website is responsive and changes depending on the screen size.

---
+++ university-site/app.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById("navLinks");
+
+//toggle menu on and off
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
